e53d915d97fb52a34827d5f721ffd5d6bb57837d
rockyb <rockyb>
2006-01-28 04:58:20 +0000

    Make sure __file__ is set.

---
+++ test/filebug.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import os
+# Make sure __file__ is defined
+# http://groups.google.com/group/comp.lang.python/browse_frm/thread/cf2a718a50f9bb52/fb754e84298513db
+print "__file__: %s" % os.path.basename(__file__)
+
