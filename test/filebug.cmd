abf8a494d461310f41d437a2ae6e3faf37d3d595
rockyb <rockyb>
2006-01-28 06:54:14 +0000

    debugger command file for __file__ test.

---
+++ test/filebug.cmd
@@ -0,0 +1,10 @@
+# Test to see __file__ is set
+# $Id: filebug.cmd,v 1.1 2006/01/28 06:54:14 rockyb Exp $
+set basename on
+set cmdtrace on
+#############################################
+## Test to see that __file__ is set.
+## Just run the program and it does the work.
+#############################################
+continue
+quit
