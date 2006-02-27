49c3465631d190608770fb3604becf9547f170cc
rockyb <rockyb>
2006-02-27 15:40:38 +0000

    Put in newly added files

---
+++ test/pm.cmd
@@ -0,0 +1,17 @@
+# Test of restart and linetracing 
+# $Id: pm.cmd,v 1.1 2006/02/27 15:40:39 rockyb Exp $
+#
+set cmdtrace on
+set interactive off
+set basename on
+show args
+info args
+list
+where
+step
+next
+finish
+return
+quit
+
+
