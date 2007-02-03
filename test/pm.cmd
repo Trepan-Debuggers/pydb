6c61cf65181bb3c826d02d99225d20cded0fcf4d
rockyb <rockyb>
2007-02-03 19:10:46 +0000

    Add regression test for recent change. Fix bug found by regression test. ;-)

--- test/pm.cmd
+++ test/pm.cmd
@@ -1,5 +1,5 @@
 # Test of restart and linetracing 
-# $Id: pm.cmd,v 1.2 2007/01/25 10:19:15 rockyb Exp $
+# $Id: pm.cmd,v 1.3 2007/02/03 19:10:47 rockyb Exp $
 #
 set trace-commands on
 set interactive off
@@ -7,6 +7,7 @@ set basename on
 show args
 info args
 list
+complete s
 where
 step
 next
